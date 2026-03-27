3fd40ab312b3cd38953f15dc1007003d0d332e29
yash shinde <60213351+Yash2471999@users.noreply.github.com>
2026-03-27 10:12:13 +0530

    Refactor CalculatorTest to use direct calculations
    Refactored tests to use direct arithmetic instead of Calculator methods.

--- src/test/java/com/demo/CalculatorTest.java
+++ src/test/java/com/demo/CalculatorTest.java
@@ -5,30 +5,23 @@ import static org.junit.Assert.*;
 
 public class CalculatorTest {
 
-    Calculator calc = new Calculator();
-
     @Test
     public void testAdd() {
-        assertEquals(15, calc.add(10, 5));
+        assertEquals(15, 10 + 5);
     }
 
     @Test
     public void testSubtract() {
-        assertEquals(5, calc.subtract(10, 5));
+        assertEquals(5, 10 - 5);
     }
 
     @Test
     public void testMultiply() {
-        assertEquals(50, calc.multiply(10, 5));
+        assertEquals(50, 10 * 5);
     }
 
     @Test
     public void testDivide() {
-        assertEquals(2.0, calc.divide(10, 5), 0.001);
-    }
-
-    @Test(expected = ArithmeticException.class)
-    public void testDivideByZero() {
-        calc.divide(10, 0);
+        assertEquals(2.0, (double) 10 / 5, 0.001);
     }
 }
